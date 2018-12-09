ae18cff4848cc57c32d9b68244e7ec0c69a2369a
jojoldu <jojoldu@gmail.com>
2018-12-09 12:25:47 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/seminar/gitflowexample/GitFlowExampleApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.seminar.gitflowexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowExampleApplication.class, args);
+    }
+}
