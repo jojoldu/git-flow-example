ab18497a766053475d90fd03f969f1b12b200e11
jojoldu <jojoldu@gmail.com>
2018-12-09 12:43:49 +0900

    코드 정리

--- src/main/java/com/jojoldu/seminar/gitflowexample/Application.java
+++ src/main/java/com/jojoldu/seminar/gitflowexample/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitFlowExampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitFlowExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
