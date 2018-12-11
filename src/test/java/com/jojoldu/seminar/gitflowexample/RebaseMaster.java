d34ff707152135de9f31973bc6a1844c9ed90141
jojoldu <jojoldu@gmail.com>
2018-12-11 09:00:01 +0900

    리베이스 마스터테스

---
+++ src/test/java/com/jojoldu/seminar/gitflowexample/RebaseMaster.java
@@ -0,0 +1,10 @@
+package com.jojoldu.seminar.gitflowexample;
+
+import org.junit.Test;
+
+public class RebaseMaster {
+    @Test
+    public void name() {
+        System.out.println("리배이스 마스터");
+    }
+}
